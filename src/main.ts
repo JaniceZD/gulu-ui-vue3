13a5493635e3ab7cf59dd79c379610d414b26d89
JaniceZD <mnzd123@gmail.com>
2022-10-25 17:19:33 +0800

    topnav 样式更新

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './index.css';
+import './index.scss';
 import { createWebHashHistory, createRouter } from 'vue-router';
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
@@ -10,7 +10,7 @@ const router = createRouter({
   history,
   routes: [
     { path: '/', component: Home },
-    { path: '/doc', component: Doc }
+    { path: '/doc', component: Doc },
   ],
 });
 const app = createApp(App);
