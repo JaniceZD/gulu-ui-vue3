b26286f51e305ba3511a3e8617a6cba206a639eb
JaniceZD <mnzd123@gmail.com>
2022-10-25 17:03:37 +0800

    搭建html结构

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
 import { createWebHashHistory, createRouter } from 'vue-router';
+import Home from './views/Home.vue';
+import Doc from './views/Doc.vue';
 
 const history = createWebHashHistory();
 const router = createRouter({
   history,
-  routes: [],
+  routes: [
+    { path: '/', component: Home },
+    { path: '/doc', component: Doc }
+  ],
 });
 const app = createApp(App);
 app.use(router);
