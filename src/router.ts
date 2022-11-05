c560119ee14a0f22b6997a6c701d8998b104519e
JaniceZD <mnzd123@gmail.com>
2022-11-05 15:39:40 +0800

    修复md文件加载问题

--- src/router.ts
+++ src/router.ts
@@ -7,10 +7,12 @@ import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
 import { h } from 'vue';
 import Markdown from './components/Markdown.vue';
+import intro from './markdown/intro.md';
+import getStarted from './markdown/get-started.md';
+import install from './markdown/install.md';
 
 const history = createWebHashHistory();
-const md = (fileName) =>
-  h(Markdown, { path: `../markdown/${fileName}.md`, key: fileName });
+const md = (string) => h(Markdown, { content: string, key: string });
 export const router = createRouter({
   history,
   routes: [
@@ -20,9 +22,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: '', redirect: '/doc/intro' },
-        { path: 'intro', component: md('intro') },
-        { path: 'install', component: md('install') },
-        { path: 'get-started', component: md('get-started') },
+        { path: 'intro', component: md(intro) },
+        { path: 'install', component: md(install) },
+        { path: 'get-started', component: md(getStarted) },
         { path: 'switch', component: SwitchDemo },
         { path: 'button', component: ButtonDemo },
         { path: 'dialog', component: DialogDemo },
