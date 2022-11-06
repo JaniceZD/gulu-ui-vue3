6fb7297386edbbc79f74b380d893f2d0be596a1b
JaniceZD <mnzd123@gmail.com>
2022-11-06 16:29:46 +0800

    创建 rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,12 @@
-// 请先安装 rollup-plugin-esbuild rollup-plugin-vue rollup-plugin-scss sass rollup-plugin-terser
 import esbuild from 'rollup-plugin-esbuild'
-import vue from 'rollup-plugin-vue'
+import vue from '@vitejs/plugin-vue'
 import scss from 'rollup-plugin-scss'
 import dartSass from 'sass';
 import { terser } from "rollup-plugin-terser"
 
 export default {
   input: 'src/lib/index.ts',
-  output: {
+  output: [{
     globals: {
       vue: 'Vue'
     },
@@ -15,7 +14,12 @@ export default {
     file: 'dist/lib/gulu.js',
     format: 'umd',
     plugins: [terser()]
-  },
+  }, {
+    name: 'Gulu',
+    file: 'dist/lib/gulu.esm.js',
+    format: 'es',
+    plugins: [terser()]
+  }],
   plugins: [
     scss({ include: /\.scss$/, sass: dartSass }),
     esbuild({
