06b265f590ce42a2a8207798aaa7f979acf42069
JaniceZD <mnzd123@gmail.com>
2022-11-06 17:22:43 +0800

    从 index 引入组件

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -14,7 +14,7 @@
   </div>
 </template>
 <script lang="ts">
-import Button from '../lib/Button.vue';
+import { Button } from '../lib/index';
 import 'prismjs';
 import 'prismjs/themes/prism.css';
 import { ref, computed } from 'vue';
