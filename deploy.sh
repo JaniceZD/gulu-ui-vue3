c89d2ce147577cfe1b1853f7df49b5919928a29b
JaniceZD <mnzd123@gmail.com>
2022-11-06 14:43:11 +0800

    修改一键部署脚本

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,6 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m "update" &&
-git branch -M master &&
-git remote add origin git@github.com:JaniceZD/gulu-ui-vue3.git &&
-git push -f -u origin master &&
+git push -f git@github.com:JaniceZD/gulu-ui-vue3.git master:gh-pages &&
 cd -
 echo https://janicezd.github.io/gulu-ui-vue3/index.html
