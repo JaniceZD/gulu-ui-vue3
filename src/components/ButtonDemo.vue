1b28b5a536f02131ad20422dc14784ca130c95d4
JaniceZD <mnzd123@gmail.com>
2023-03-19 16:56:04 +0800

    添加属性说明

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -6,6 +6,7 @@
     <Demo :component="Button2Demo" />
     <Demo :component="Button4Demo" />
     <Demo :component="Button5Demo" />
+    <Attr :data="data"></Attr>
   </div>
 </template>
 <script lang="ts">
@@ -15,12 +16,59 @@ import Button2Demo from './Button2.demo.vue';
 import Button3Demo from './Button3.demo.vue';
 import Button4Demo from './Button4.demo.vue';
 import Button5Demo from './Button5.demo.vue';
+import { ref } from 'vue';
+import { Attr } from '../lib/index';
 export default {
   components: {
     Demo,
+    Attr,
   },
   setup() {
-    return { Button1Demo, Button2Demo, Button3Demo, Button4Demo, Button5Demo };
+    const data = ref([
+      {
+        params: 'theme',
+        desc: '按钮类型',
+        type: 'string',
+        select: 'button / link / text',
+        default: 'button',
+      },
+      {
+        params: 'level',
+        desc: '按钮颜色',
+        type: 'string',
+        select: 'main / warning / success / danger / info',
+        default: 'default',
+      },
+      {
+        params: 'size',
+        desc: '尺寸大小',
+        type: 'string',
+        select: 'small / normal / big',
+        default: 'normal',
+      },
+      {
+        params: 'disabled',
+        desc: '是否禁用',
+        type: 'boolean',
+        select: 'false / true',
+        default: 'false',
+      },
+      {
+        params: 'loading',
+        desc: '是否加载',
+        type: 'boolean',
+        select: 'false / true',
+        default: 'false',
+      },
+    ]);
+    return {
+      Button1Demo,
+      Button2Demo,
+      Button3Demo,
+      Button4Demo,
+      Button5Demo,
+      data,
+    };
   },
 };
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -18,7 +18,7 @@ export default {
     },
     level: {
       type: String,
-      default: 'normal',
+      default: 'default',
     },
     disabled: {
       type: Boolean,
